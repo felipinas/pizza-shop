9b4c7b02d2ef3e2400a75833d6bcb72e87bc931b
felipinas <joaofelipemail99@gmail.com>
2024-03-14 10:03:28 -0300

    feat: add react hook form and sonner api

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,45 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>({
+    resolver: zodResolver(signInForm),
+  });
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      console.log(data);
+
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      toast.success("Enviamos um link de autenticação para seu e-mail.", {
+        action: {
+          label: "Reenviar",
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch (error) {
+      toast.error("Credenciais inválidas.");
+    }
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -21,14 +56,14 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
 
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register("email")} />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
               Acessar painel
             </Button>
           </form>
